d7d5d8cfecda107d8557ed90eb9d9d865e30f9ce
Vladimir Golub <golubvlauri@gmail.com>
2020-06-29 17:07:56 +0200

    fix position

--- pages/index.vue
+++ pages/index.vue
@@ -9,6 +9,11 @@
         ref="pageElement"
       ></div>
     </div>
+    <div
+      class="page__container"
+      ref="pageSection"
+    >
+    </div>
   </div>
 </template>
 
@@ -22,13 +27,17 @@ export default {
     const scene1 = new this.$scrollmagic.Scene({
       triggerElement: this.$refs.pageContainer,
       triggerHook: 1,
-      duration: containerSizes.height - 100,
+      duration: containerSizes.height,
       offset: 100
     })
       .on('progress', e => {
-        console.log(e.progress);
 
-        this.$refs.pageElement.style.top = `${ e.progress * ( containerSizes.height - 100 ) }px`;
+        if (e.progress == 1 || e.progress == 0) {
+          this.$refs.pageElement.style.position = 'absolute'
+        } else {
+          this.$refs.pageElement.style.position = 'fixed';
+        }
+
       });
 
     controller.addScene([scene1]);
@@ -54,10 +63,8 @@ export default {
       background-image: url("~static/businessman.jpg");
       background-repeat: no-repeat;
       background-size: contain;
-      position: absolute;
-      top: 0;
-      left: 50%;
-      transform: translateX(-50%);
+      position: fixed;
+      bottom: 0;
     }
   }
 </style>
