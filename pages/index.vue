e8a481cb50469405d73d463dd2fe2a8bbd1e7b1f
Vladimir Golub <golubvlauri@gmail.com>
2020-06-30 09:55:02 +0200

    add position reaction on scroll

--- pages/index.vue
+++ pages/index.vue
@@ -5,38 +5,50 @@
       ref="pageContainer"
     >
       <div
-        class="page__element"
+        :class="{
+          'page__element': true,
+          'page__element--top': top,
+          'page__element--bottom': bottom,
+          'page__element--fixed': fixed
+        }"
         ref="pageElement"
       ></div>
-    </div>
-    <div
-      class="page__container"
-      ref="pageSection"
-    >
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  data: function() {
+    return {
+      top: true,
+      bottom: false,
+      fixed: false
+    }
+  },
   mounted() {
     const containerSizes = this.$refs.pageContainer.getBoundingClientRect();
+    const elementSizes = this.$refs.pageElement.getBoundingClientRect();
 
     const controller = new this.$scrollmagic.Controller();
 
     const scene1 = new this.$scrollmagic.Scene({
       triggerElement: this.$refs.pageContainer,
       triggerHook: 1,
-      duration: containerSizes.height,
+      duration: containerSizes.height - elementSizes.height,
+      offset: elementSizes.height
     })
       .on('progress', e => {
 
         if (e.progress == 1 || e.progress == 0) {
-          this.$refs.pageElement.style.position = 'absolute'
+          this.fixed = false;
+          this.top = e.progress == 0 ? true : false;
+          this.bottom = e.progress == 1 ? true : false;
         } else {
-          this.$refs.pageElement.style.position = 'fixed';
+          this.fixed = true;
+          this.top = false;
+          this.bottom = false;
         }
-
       });
 
     controller.addScene([scene1]);
@@ -50,7 +62,7 @@ export default {
     &__container {
       height: 100vh;
       //width: 900px;
-      margin: 100vh auto 0;
+      margin: 100vh auto 100vh;
       background: #EEEEEE;
       position: relative;
     }
@@ -62,8 +74,25 @@ export default {
       background-image: url("~static/businessman.jpg");
       background-repeat: no-repeat;
       background-size: contain;
-      position: fixed;
-      bottom: 0;
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+
+      &--top {
+        position: absolute;
+        top: 0;
+      }
+
+      &--bottom {
+        position: absolute;
+        bottom: 0;
+      }
+
+      &--fixed {
+        position: fixed;
+        bottom: 0;
+      }
+
     }
   }
 </style>
